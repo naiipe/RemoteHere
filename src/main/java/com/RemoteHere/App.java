33811db79723e45564ab6af07dcf4c8a3b806f10
Rafael <rfarias@mcfadyen.com>
2019-02-28 20:41:39 -0300

    RemoteOK API request implemented

--- src/main/java/com/RemoteHere/App.java
+++ src/main/java/com/RemoteHere/App.java
@@ -1,9 +1,12 @@
 package com.RemoteHere;
 
+import java.io.IOException;
+
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
-		Email.sendEmail();
+		//Email.sendEmail();
+		//JobController.getJobsList();
 	}
 }
--- src/main/java/com/RemoteHere/JobController.java
+++ src/main/java/com/RemoteHere/JobController.java
@@ -1,6 +1,27 @@
 package com.RemoteHere;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class JobController {
 
+	public static Object getJobsList() throws IOException {
+
+		URL url = new URL("https://remoteok.io/api");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		con.setRequestMethod("GET");
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+		System.out.println(content);
+		in.close();
+		return content;
+	}
 }
