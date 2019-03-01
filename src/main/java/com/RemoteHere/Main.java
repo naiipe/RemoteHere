8ffb321a66bc2574149303c87b770ca03b92ac4f
unknown <mateuspbitt2@gmail.com>
2019-03-01 00:54:32 -0300

    Acesso a base do Json e criado o laço para acesso de todos os anucíos

---
+++ src/main/java/com/RemoteHere/Main.java
@@ -0,0 +1,68 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Main {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args)
+	{       
+		JSONParser jsonParser = new JSONParser();
+
+
+		//LEITURA DA BASE DE DADOS DO JSON LOCA
+		try (FileReader reader = new FileReader("C:/data.json"))
+		{
+			Object obj = jsonParser.parse(reader);
+
+			JSONArray dataJson = (JSONArray) obj;
+			ArrayList<Object> dados = new ArrayList<Object>();
+			//CRIA UM ARRAY COM TODOS OS ANUNCIOS
+			for(int i = 0; i < dataJson.size(); i++) {                	
+				dados.add(dataJson.get(i));           
+			}
+			//IMPRIME TODOS OS ANUNCIOS
+			System.out.println(dados);  
+
+			//PEGAR ATRIBUTOS DE CADA ITEM DO ARRAY
+		} catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		} catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
